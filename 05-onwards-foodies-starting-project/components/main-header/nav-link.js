ffd08bf8cceaa1d48785b6c711fbdd8f97ccea69
Nitheesh KR <nitheeshkr115@gmail.com>
2025-10-14 05:41:09 +0200

    created a nav link component to use the use client in smaller components

---
+++ 05-onwards-foodies-starting-project/components/main-header/nav-link.js
@@ -0,0 +1,19 @@
+"use client";
+
+import Link from "next/link";
+import classes from "./nav-link.module.css";
+import { usePathname } from "next/navigation";
+
+export default function NavLink({ href, children }) {
+  const path = usePathname();
+  return (
+    <Link
+      href={href}
+      className={`${classes.link} ${
+        path.startsWith(href) ? classes.active : ""
+      }`}
+    >
+      {children}
+    </Link>
+  );
+}
