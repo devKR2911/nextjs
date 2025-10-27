8c623cee587b22262d8a413554c8d3eab967e654
Nitheesh KR <nitheeshkr115@gmail.com>
2025-10-27 06:23:53 +0100

    using form status hook from react to show form submission status

--- 05-onwards-foodies-starting-project/app/meals/share/page.js
+++ 05-onwards-foodies-starting-project/app/meals/share/page.js
@@ -1,6 +1,7 @@
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/action";
+import MealsSubmitButton from "@/components/meals/meals-submit-button";
 
 export default function ShareMealPage() {
   return (
@@ -42,7 +43,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker name="image" label="Select an image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsSubmitButton />
           </p>
         </form>
       </main>
