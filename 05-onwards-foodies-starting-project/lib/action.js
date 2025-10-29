2adc11e7c3e7d70f139b35b44d54933fe385edb5
Nitheesh KR <nitheeshkr115@gmail.com>
2025-10-29 07:02:15 +0100

    adding server side validation for share meal form

--- 05-onwards-foodies-starting-project/lib/action.js
+++ 05-onwards-foodies-starting-project/lib/action.js
@@ -3,6 +3,10 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
+const isInvalidText = (text) => {
+  return !text || text.trim() === "";
+};
+
 export async function shareMeal(formData) {
   const mealData = {
     title: formData.get("title"),
@@ -12,6 +16,19 @@ export async function shareMeal(formData) {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+
+  if (
+    isInvalidText(mealData.title) ||
+    isInvalidText(mealData.summary) ||
+    isInvalidText(mealData.instructions) ||
+    isInvalidText(mealData.creator) ||
+    isInvalidText(mealData.creator_email) ||
+    !mealData.creator_email.includes("@") ||
+    !mealData.image ||
+    mealData.image.size === 0
+  ) {
+    throw new Error("Invalid input");
+  }
   await saveMeal(mealData);
   redirect("/meals");
 }
