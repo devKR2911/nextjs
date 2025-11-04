be25844f6e00137110829421dee439b8ebb3efbb
Nitheesh Keerthi Veettil Ramakrishnan <nitheesh.r@tryg.no>
2025-11-04 09:04:20 +0100

    added not found page

---
+++ nextjs-routing/app/news/[slug]/not-found.js
@@ -0,0 +1,6 @@
+export default function NewsNotFound() {
+    return (<div id="error">
+        <h1>News found!!</h1>
+        <p>The requested news cannot be found</p>
+    </div>)
+}
---
+++ nextjs-routing/app/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFound() {
+    return (<div id="error">
+        <h1>Not found!!</h1>
+        <p>The requested resource cannot be found</p>
+    </div>)
+}
