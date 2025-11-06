d7d42b028570d1279c6018d4365b13ae1eb2fa7b
Nitheesh Keerthi Veettil Ramakrishnan <nitheesh.r@tryg.no>
2025-11-06 06:47:24 +0100

    adding parallel routes into the app

--- nextjs-routing/app/news/page.js
+++ nextjs-routing/app/news/page.js
@@ -1,22 +1,12 @@
-import Link from "next/link";
-
 import { DUMMY_NEWS } from "@/dummy-news";
+import NewsList from "@/components/news-list/news-list";
 
 export default function NewsPage() {
   return (
     <div>
       <h1>The NEWS Page</h1>
       <div>
-        <ul className="news-list">
-          {DUMMY_NEWS.map((news) => (
-            <li key={news.id}>
-              <Link href={`/news/${news.slug}`}>
-                <img src={`/images/news/${news.image}`} alt={news.title} />
-                <span>{news.title}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <NewsList news={DUMMY_NEWS}></NewsList>
       </div>
     </div>
   );
